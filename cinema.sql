b7664e334b10ee7f49395a59c1ac611be5b8143f
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-06-13 16:26:01 +0200

    First commit : DB created

---
+++ cinema.sql
@@ -0,0 +1,13 @@
+-- connexion au serveur MySQL
+
+mysql -u root -p 
+
+-- Afficher la liste des bases de données
+SHOW databases;
+
+-- Création de la  base de données
+CREATE DATABASE  cinema_booking ;
+
+-- Sélectionner la bbd à utiliser pour la création de table
+USE cinema_booking ;
+
